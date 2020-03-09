988a365e36c168f3dc1b36365af4e7da37f53dc2
shankze <shankze@gmail.com>
2020-03-08 22:03:23 -0500

    Add Routing

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,18 +10,9 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  /*hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };*/
   heroes: Hero[];
 
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService,private messageService: MessageService) { }
 
   getHeroes(): void {
     this.heroService.getHeroes()
